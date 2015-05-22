d03cf337dcb76c20804cdc68396dd0c641a3df94
Jonathan Mariano <jamarian@ucsd.edu>
2015-05-21 23:19:58 -0700

    Remove Duplicates from chargeArray

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,35 @@
 var DelphiDemo = DelphiDemo || (function() {
   var self = {};
   var chargeArray = new Array();
+  var distinct;//use for bar chart x-axis
+  
   /** 
    * Send an ajax request to the server to retrieve delphi db data.
    */ 
   self.getDelphiData = function() {
     $.getJSON("/delphidata", function(data) {
       var rows = $.map(data, function (item, i) {
-      
+      	
+      	//Array of ALL charge descriptions for peticular zip code from app.js query
       	chargeArray.push(item.charge_description);
+      
+      	//Distinct array of charge descriptions
+      	//Remove duplicate charge descriptions 
+      	distinct = chargeArray.filter(function(elem, pos) {
+		  	return chargeArray.indexOf(elem) == pos;
+		});
+      	
+      	
       	
         return "<tr><td>" + item.agency + "</td><td>" + item.charge_description + "</td><td>" + item.activity_date + "</td><td>" + item.block_address + "</td><td>" + item.zip + "</td><td>" + item.community + "</td></tr>";
       }).join("");
       
-      console.log(chargeArray);
+      console.log("chargeArray " + chargeArray);
+      console.log("chargeArray LENGTH " + chargeArray.length);
+      
+      console.log("distinct " + distinct);
+      console.log("distinct LENGTH " + distinct.length);
+
 
 
       $("#delphi-table").append(rows);
